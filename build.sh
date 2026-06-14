f6dfc56c16f1f6bb925b27474cbbdf9ea00ea7ad
jim3692 <jim3692@gmail.com>
2026-06-14 14:57:34 +0300

    fix: sign entire derivation recursively

--- build.sh
+++ build.sh
@@ -13,27 +13,13 @@ mkdir -p build
 do_handle_der() {
   der="$1"
 
-  while read store_path; do
-    do_verify "$store_path" && continue
-    do_sign "$store_path"
-  done < <(nix path-info -r "$der")
+  nix store sign --key-file nix-private.pem --recursive "$der"
 
   nix copy --to "ssh-ng://cache-server" "$der"
 
   gc_script="$(nix-store --add ./utils/register-gc-root.sh)"
   nix copy --to "ssh-ng://cache-server" "$gc_script"
   ssh cache-server bash "$gc_script" "$der"
-}
-
-do_sign() {
-  nix store sign --key-file nix-private.pem "$1"
-}
-
-do_verify() {
-  nix store verify \
-    --no-contents \
-    --sigs-needed 1 "$1" \
-    --option extra-trusted-public-keys "$(cat nix-public.pem)"
 }
 
 icedos_out="$PWD/build/icedos"
