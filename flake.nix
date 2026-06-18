410aa58209513c99e6e240945c50f72616d7553c
jim3692 <jim3692@gmail.com>
2026-06-18 23:31:02 +0300

    feat(flake): implement devShell

--- flake.nix
+++ flake.nix
@@ -22,7 +22,9 @@
       pkgs = nixpkgs.legacyPackages.${system};
 
       dockerStackPkg = pkgs.writeShellScriptBin "docker" ''
-        env ATTICD_BIN="${pkgs.attic-server}/bin/atticd" \
+        env \
+          ATTICD_BIN="${pkgs.attic-server}/bin/atticd" \
+          ATTIC_SERVER_TOKEN_HS256_SECRET_BASE64="$(sudo cat /etc/icedos-attic-secret)" \
           docker compose -f "${self}/stack/compose.yml" "$@"
       '';
 
@@ -45,6 +47,33 @@
         };
 
         icedos = icedosApp;
+      };
+
+      devShells.${system} = {
+        stack = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            attic-client
+            attic-server
+          ];
+          shellHook = ''
+            export ATTICD_BIN="${pkgs.attic-server}/bin/atticd"
+            export ATTIC_SERVER_TOKEN_HS256_SECRET_BASE64="$(sudo cat /etc/icedos-attic-secret)"
+
+            generate_attic_admin_token() {
+              nix run .#stack.docker -- exec app "${pkgs.attic-server}/bin/atticadm" \
+                -f /etc/attic/server.toml \
+                make-token --sub admin --validity '100y' \
+                --pull '*' --push '*' --create-cache '*' --configure-cache '*' --configure-cache-retention '*'
+            }
+
+            generate_attic_builder_token() {
+              nix run .#stack.docker -- exec app "${pkgs.attic-server}/bin/atticadm" \
+                -f /etc/attic/server.toml \
+                make-token --sub ci --validity '1y' \
+                --pull icedos --push icedos
+            }
+          '';
+        };
       };
 
       packages.${system} = {
